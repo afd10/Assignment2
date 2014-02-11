0d8a0e4bc2e5b962f8e3bc9c96e37ba83049350f
Alayne Doney <alaynedoney@bx-wireless-pittnet-150-212-28-162.wireless.pitt.edu>
2014-02-11 14:40:55 -0500

    Adding vegetable.h and vegetable.m and screenshot

--- Fruit/ViewController.m
+++ Fruit/ViewController.m
@@ -1,13 +1,13 @@
 //
 //  ViewController.m
-//  Fruit
+//  Vegetable
 //
 //  Created by Build User on 1/28/14.
 //  Copyright (c) 2014 Pitt. All rights reserved.
 //
 
 #import "ViewController.h"
-#import "Fruit.h"
+#import "Vegetable.h"
 
 @interface ViewController ()
 @property (nonatomic, strong) NSMutableArray *cart;
@@ -22,12 +22,26 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.cart = [[NSMutableArray alloc] init];
     NSString *itemName;
-    NSString *fruitName = @"Bananas";
+    NSString *vegetableName= @"Potato";
+    NSString *vegetableName2= @"Onion";
+    NSString *vegetableName3= @"Pumpkin";
     
-    for (int i = 0; i<50; i++) {
-        itemName = [[NSString alloc] initWithFormat:@"%@ %d", fruitName, i];
-        Fruit *tempFruit = [[Fruit alloc] initWithName:itemName andShape:@"Curved" andColor:@"Yellow"];
-        [self.cart addObject:tempFruit];
+    for (int i = 0; i<2; i++) {
+        itemName = [[NSString alloc] initWithFormat:@"%@ %d", vegetableName, i];
+        Vegetable *tempVegetable = [[Vegetable alloc] initWithName:itemName andShape:@"Curved" andColor:@"Yellow"];
+        [self.cart addObject:tempVegetable];
+    }
+    
+    for (int i = 0; i<2; i++) {
+        itemName = [[NSString alloc] initWithFormat:@"%@ %d", vegetableName2, i];
+        Vegetable *tempVegetable = [[Vegetable alloc] initWithName:itemName andShape:@"Curved" andColor:@"Yellow"];
+        [self.cart addObject:tempVegetable];
+    }
+    
+    for (int i = 0; i<2; i++) {
+        itemName = [[NSString alloc] initWithFormat:@"%@ %d", vegetableName3, i];
+        Vegetable *tempVegetable = [[Vegetable alloc] initWithName:itemName andShape:@"Curved" andColor:@"Yellow"];
+        [self.cart addObject:tempVegetable];
     }
 }
 
@@ -44,7 +58,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    NSString *identifier = @"fruitCell";
+    NSString *identifier = @"vegetableCell";
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
     
     cell.textLabel.text = [[self.cart objectAtIndex:[indexPath row]] name];
